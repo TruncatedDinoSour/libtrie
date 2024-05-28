d10fe4e8835b4fcdc0dfc638654f13d8d0ef84b9
Ari Archer <ari@ari.lt>
2024-05-28 19:16:28 +0300

    Improve the RNG, ignore \r
    Signed-off-by: Ari Archer <ari@ari.lt>

--- src/rng.c
+++ src/rng.c
@@ -3,16 +3,24 @@
 void rng_seed(TrieRNG *rng, const uint8_t *seed, const uint64_t seed_size) {
     uint64_t idx;
 
-    rng->state = 0;
+    rng->state = 0x4D595DF4;
 
-    for (idx = 0; idx < seed_size; ++idx)
-        rng->state = (rng->state << 8) | seed[idx];
+    for (idx = 0; idx < seed_size; ++idx) {
+        rng->state ^= (uint32_t)(seed[idx] << ((idx % 4) * 8));
+        rng->state ^= (rng->state << 13);
+        rng->state ^= (rng->state >> 17);
+        rng->state ^= (rng->state << 5);
+    }
 
-    rng->state |= 1;
+    if (rng->state == 0)
+        rng->state = 0x4D595DF4;
 }
 
 uint32_t rng_next(TrieRNG *rng) {
-    rng->state =
-        (uint32_t)(rng->state * 2854097789 + 3968177761); /* 2 random primes */
-    return (uint32_t)(rng->state % (RNG_MAX - 1));
+    uint32_t x = rng->state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    rng->state = x;
+    return x;
 }
--- src/rng.h
+++ src/rng.h
@@ -12,9 +12,8 @@ typedef struct TrieRNG {
 #define ITER_MAGIC (uint32_t)2170632407 /* Random prime number */
 #endif                                  /* ITER_MAGIC */
 
-#define rng_iter(state, count, idx)                                          \
-    (state) += (uint32_t)((((count) - (idx) + ITER_MAGIC + (idx % 2 == 0)) % \
-                           RNG_MAX))
+#define rng_iter(state, count, idx) \
+    (state) ^= ((count) - (idx) + ITER_MAGIC + ((idx) % 2 == 0))
 
 void rng_seed(TrieRNG *rng, const uint8_t *seed, const uint64_t seed_size);
 uint32_t rng_next(TrieRNG *rng);
